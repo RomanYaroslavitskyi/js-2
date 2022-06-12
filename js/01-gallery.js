2501f8c96a4ef31064e02aede7f50257c20648ea
Roman <Chugaisters@gmail.com>
2022-06-12 12:54:26 +0300

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const paletteContainer = document.querySelector(".gallery");
+let cardsMarkup = createGalleryMarkup(galleryItems);
+
+paletteContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+const imageDivs = document.querySelectorAll('.gallery__image');
+
+
+imageDivs.forEach(function (image) {
+    image.addEventListener("click", onPaletteContainerClick);
+});
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(item => {
+        return `
+  <div class="gallery__item">
+  <a class="gallery__link" href=${item.preview}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>
+</div>
+`;
+    })
+        .join(" ");
+}
+function onPaletteContainerClick(evt) {
+
+    evt.preventDefault();
+    const image = this;
+    let dataSourseAtr = image.getAttribute('data-source');
+
+     window.instance = basicLightbox.create(`
+    <img src=${dataSourseAtr} width="800" height="600">
+`);
+    if (!evt.target.classList.contains("gallery__image")) {
+        return;
+    } else {
+
+        window.instance.show();
+    }
+    window.onkeydown = function (event) {
+        if (event.keyCode == 27) {
+            if (window.instance.visible()) {
+                window.instance.close();
+            }
+        }
+    };
+
+}
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const paletteContainer = document.querySelector(".gallery");
+let galleryMarkup = createGalleryMarkup(galleryItems);
+
+paletteContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+var lightbox = new SimpleLightbox('.gallery__item a', { nav: true, captionDelay: 250 });
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(item => {
+        return `
+  <div class="gallery__item">
+  <a class="gallery__link" title="${item.description}" href=${item.preview}>
+    <img
+      class="gallery__image"
+      src=${item.original}
+      title=${item.description}
+    />
+  </a>
+</div>
+`;
+
+    })
+        .join(" ");
+}
+
+
