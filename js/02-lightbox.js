a1d58cab31e66d9f207e076008c0bbae774a0bf7
Roman <Chugaisters@gmail.com>
2022-06-29 16:43:15 +0300

    finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const paletteContainer = document.querySelector(".gallery");
@@ -6,24 +6,25 @@ let galleryMarkup = createGalleryMarkup(galleryItems);
 
 paletteContainer.insertAdjacentHTML("beforeend", galleryMarkup);
 
-var lightbox = new SimpleLightbox('.gallery__item a', { nav: true, captionDelay: 250 });
+var lightbox = new SimpleLightbox(".gallery__item a", {
+  nav: true,
+  captionDelay: 250,
+});
 
 function createGalleryMarkup(galleryItems) {
-    return galleryItems.map(item => {
-        return `
+  return galleryItems
+    .map((item) => {
+      return `
   <div class="gallery__item">
-  <a class="gallery__link" title="${item.description}" href=${item.preview}>
+  <a class="gallery__link" href=${item.original}>
     <img
       class="gallery__image"
-      src=${item.original}
-      title=${item.description}
+      src=${item.preview}
+      alt=${item.description}
     />
   </a>
 </div>
 `;
-
     })
-        .join(" ");
+    .join(" ");
 }
-
-
